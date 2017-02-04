1d860e97c938e6d40824a17d488e0e5f75711dcd
Clinton H. Durney <cdurney89@gmail.com>
2017-02-04 13:13:36 -0800

    update

--- Biochemistry/dde_main.py
+++ Biochemistry/dde_main.py
@@ -1,7 +1,4 @@
 # biochemical signaling model 
-# testing of uploading a change to github
-
-# import pydelay and numpy and pylab
 
 from __future__ import absolute_import
 import numpy as np
@@ -14,32 +11,32 @@ import json
 # the model equations 
 eqns = { 
     'Ac' : '-k1*Rm*Ac',
-    'Am' : 'k1*Rm(t-tau1)*Ac(t-tau1)*Heavi(t-tau1) - k2*Am*Rm + k2*AR - k2*Am*Bm + k3*AB',
+    'Am' : 'k1*Rm(t-tau1)*Ac(t-tau1) - k2*Am*Rm + k3*AR - k2*Am*Bm + k3*AB',
     'Bc' : '-k4*Am*Bc',
-    'Bm' : 'k4*Am(t-tau2)*Bc(t-tau2)*Heavi(t-tau2) - k2*Am*Bm + k3*AB',
-    'Rm' : 'qR - k2*Am*Rm + k2*AR',
+    'Bm' : 'k4*Am(t-tau2)*Bc(t-tau2) - k2*Am*Bm + k3*AB',
+    'Rm' : 'qR - k2*Am*Rm + k3*AR',
     'AB' : 'k2*Am*Bm - k3*AB',
-    'AR' : 'k2*Am*Rm - k2*AR'
+    'AR' : 'k2*Am*Rm - k3*AR'
     }
 
 # define parameters
 params = {
-    'k1' : 0.000003,           
-    'k2' : 0.15,
-    'k3' : 0.009,
-    'k4' : 0.000003,
-    'qR' : 8,
-    'tau1' : 75,
-    'tau2' : 80
+    'k1' : 0.001,           
+    'k2' : 0.5,
+    'k3' : 0.0001,
+    'k4' : 0.001,
+    'qR' : 0.1,
+    'tau1' : 40.,
+    'tau2' : 40.
     }
 
 # initial conditions
 init_cond = {
-    'Ac' : 60000.,
+    'Ac' : 1000.,
     'Am' : 0,
-    'Bc' : 4000.,
+    'Bc' : 500.,
     'Bm' : 0,
-    'Rm' : 500.,
+    'Rm' : 0.,
     'AB' : 0,
     'AR' : 0
     }
@@ -90,10 +87,10 @@ AR = sol1['AR']
 
 # print the IC's and constants
 print("The parameters used were:")
-print(json.dumps(params, indent = 1))
+print(json.dumps(params, indent = 1, sort_keys=True))
 
 print("The initial conditions used were: ")
-print(json.dumps(init_cond, indent = 1))
+print(json.dumps(init_cond, indent = 1, sort_keys=True))
 
 
 # Create plots 
@@ -121,6 +118,7 @@ plt.ylabel("$\mu$M")
 plt.figure(4)
 plt.title("Medial Reg, aPKC, Baz")
 pl.xlim([t[0],t[-1]])
+pl.ylim([0,1])
 plt.plot(t, Rm/np.amax(Rm),'r', label = "$Reg_m$")
 plt.plot(t,Am/np.amax(Am), 'g', label = "$aPKC_m$")
 plt.plot(t,Bm/np.amax(Bm), 'b', label = "$Baz_m$")
