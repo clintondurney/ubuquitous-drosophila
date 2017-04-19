0f6ffab939df9cec5bf631e06ec8afc2d687a249
Clinton H. Durney <cdurney89@gmail.com>
2017-04-19 16:50:10 -0700

    25 cells

--- MultCell/dde_main.py
+++ MultCell/dde_main.py
@@ -11,6 +11,10 @@ from pydelay import dde23
 import globals as const
 import csv
 import json
+import networkx as nx
+import itertools
+
+
 
 def dde_initializer(Ac_i,Am_i,Bc_i,Bm_i,Rm_i,AB_i,AR_i,tf,dt):
     # the model equations 
@@ -187,16 +191,15 @@ def kminus(myo,length):
 ################
 def fun(y,signal,length):
     # needs updated for geo distribution
-    geo_frac = 1                # evenly distributed to start (single cell)
     
-    return(const.k_plus*signal*geo_frac - kminus(y,length)*y)
+    return(const.k_plus*signal - kminus(y,length)*y)
 
 ################
 def dmyosin(y,Reg,length, dt):
 
     return(y+dt*fun(y,Reg,length))
 
-
+################
 def CellArea(corners):
     n = len(corners) # of corners
     area = 0.0
@@ -206,4 +209,107 @@ def CellArea(corners):
         area -= corners[j][0] * corners[i][1]
     area = abs(area) / 2.0
     return area
+
+################
+def conc2mol(IC, cell_vol):
+    Mol = IC*10**(-3)*10**(-12)*6.022*10**(23)*cell_vol
+    return Mol
+
+################
+def tissue():
+    r = const.l_initial
+    xx = np.arange(0,15*r,r)
+    yy = np.arange(0,11*r*np.sqrt(3)*0.5,r*np.sqrt(3)*0.5)
+    points = list(itertools.product(xx,yy))
+
+    # shear the points
+    m = np.sqrt(3)/3
+
+    new_points = []
+    for i in range(0,len(points)):
+        x = points[i][0] + m*points[i][1]
+        y = points[i][1]
+        new_points.append((x,y))  
+
+    points = new_points
+
+    G = nx.Graph()
+    for i in range(0,len(points)):
+        G.add_node(i,pos=points[i])
+
+    section_1 = [0,1,2,3,4,11,12,13,14,22,23,24,25,33,34,35,44,45,46,55,56,66,67,77,88]
+    section_2 = [121,132,133,143,144,145,154,155,156,157]
+    section_3 = [7,8,9,10,19,20,21,31,32,43]
+    section_4 = [76,87,98,109,120,131,142,153,164,97,108,119,130,141,152,163,118,129,140,151,162,139,150,161,160]
+
+    G.remove_nodes_from(section_1)
+    G.remove_nodes_from(section_2)
+    G.remove_nodes_from(section_3)
+    G.remove_nodes_from(section_4)
+
+    G = nx.convert_node_labels_to_integers(G,first_label=0)
+
+    G.add_path([0,1,4,7,6,2,0],beta=0,myosin=0,color='r')
+    G.add_path([6,11,18,19,13,7],beta=0,myosin=0,color='r')
+    G.add_path([18,26,35,36,28,19],beta=0,myosin=0,color='r')
+    G.add_path([4,5,9,14,13],beta=0,myosin=0,color='r')
+    G.add_path([14,21,29,28],beta=0,myosin=0,color='r')
+    G.add_path([9,10,16,22,21],beta=0,myosin=0,color='r')
+    G.add_path([22,31,39,38,29],beta=0,myosin=0,color='r')
+    G.add_path([38,46,45,36],beta=0,myosin=0,color='r')
+    G.add_path([35,43,52,53,45],beta=0,myosin=0,color='r')
+    G.add_path([31,32,24,17,16],beta=0,myosin=0,color='r')
+    G.add_path([32,41,42,34,25,24],beta=0,myosin=0,color='r')
+    G.add_path([39,48,49,41],beta=0,myosin=0,color='r')
+    G.add_path([46,55,56,48],beta=0,myosin=0,color='r')
+    G.add_path([53,62,63,55],beta=0,myosin=0,color='r')
+    G.add_path([52,60,69,70,62],beta=0,myosin=0,color='r')
+    G.add_path([70,77,78,72,63],beta=0,myosin=0,color='r')
+    G.add_path([72,73,65,56],beta=0,myosin=0,color='r')
+    G.add_path([65,66,58,49],beta=0,myosin=0,color='r')
+    G.add_path([58,59,51,42],beta=0,myosin=0,color='r')
+    G.add_path([66,75,76,68,59],beta=0,myosin=0,color='r')
+    G.add_path([73,80,81,75],beta=0,myosin=0,color='r')
+    G.add_path([78,84,85,80],beta=0,myosin=0,color='r')
+    G.add_path([81,87,88,83,76],beta=0,myosin=0,color='r')
+    G.add_path([85,89,90,87],beta=0,myosin=0,color='r')
+    G.add_path([90,93,94,92,88],beta=0,myosin=0,color='r')
+
+
+    G.add_edges_from([(3,0),(3,1),(3,4),(3,7),(3,6),(3,2)],beta=const.beta,myosin=const.myo0)
+    G.add_edges_from([(8,14),(8,9),(8,5),(8,4),(8,7),(8,13)],beta=const.beta,myosin=const.myo0)
+    G.add_edges_from([(12,19),(12,13),(12,7),(12,6),(12,11),(12,18)],beta=const.beta,myosin=const.myo0)
+    G.add_edges_from([(15,22),(15,16),(15,10),(15,9),(15,14),(15,21)],beta=const.beta,myosin=const.myo0)
+    G.add_edges_from([(20,29),(20,21),(20,14),(20,13),(20,19),(20,28)],beta=const.beta,myosin=const.myo0)
+    G.add_edges_from([(27,36),(27,28),(27,19),(27,18),(27,26),(27,35)],beta=const.beta,myosin=const.myo0)
+    G.add_edges_from([(23,32),(23,24),(23,17),(23,16),(23,22),(23,31)],beta=const.beta,myosin=const.myo0)
+    G.add_edges_from([(30,39),(30,31),(30,22),(30,21),(30,29),(30,38)],beta=const.beta,myosin=const.myo0)
+    G.add_edges_from([(37,46),(37,38),(37,29),(37,28),(37,36),(37,45)],beta=const.beta,myosin=const.myo0)
+    G.add_edges_from([(44,53),(44,45),(44,36),(44,35),(44,43),(44,52)],beta=const.beta,myosin=const.myo0)
+    G.add_edges_from([(33,42),(33,34),(33,25),(33,24),(33,32),(33,41)],beta=const.beta,myosin=const.myo0)
+    G.add_edges_from([(40,49),(40,41),(40,32),(40,31),(40,39),(40,48)],beta=const.beta,myosin=const.myo0)
+    G.add_edges_from([(47,56),(47,48),(47,39),(47,38),(47,46),(47,55)],beta=const.beta,myosin=const.myo0)
+    G.add_edges_from([(54,63),(54,55),(54,46),(54,45),(54,53),(54,62)],beta=const.beta,myosin=const.myo0)
+    G.add_edges_from([(61,70),(61,62),(61,53),(61,52),(61,60),(61,69)],beta=const.beta,myosin=const.myo0)
+    G.add_edges_from([(50,59),(50,51),(50,42),(50,41),(50,49),(50,58)],beta=const.beta,myosin=const.myo0)
+    G.add_edges_from([(57,66),(57,58),(57,49),(57,48),(57,56),(57,65)],beta=const.beta,myosin=const.myo0)
+    G.add_edges_from([(64,73),(64,65),(64,56),(64,55),(64,63),(64,72)],beta=const.beta,myosin=const.myo0)
+    G.add_edges_from([(71,78),(71,72),(71,63),(71,62),(71,70),(71,77)],beta=const.beta,myosin=const.myo0)
+    G.add_edges_from([(67,76),(67,68),(67,59),(67,58),(67,66),(67,75)],beta=const.beta,myosin=const.myo0)
+    G.add_edges_from([(74,81),(74,75),(74,66),(74,65),(74,73),(74,80)],beta=const.beta,myosin=const.myo0)
+    G.add_edges_from([(79,85),(79,80),(79,73),(79,72),(79,78),(79,84)],beta=const.beta,myosin=const.myo0)
+    G.add_edges_from([(82,88),(82,83),(82,76),(82,75),(82,81),(82,87)],beta=const.beta,myosin=const.myo0)
+    G.add_edges_from([(86,90),(86,87),(86,81),(86,80),(86,85),(86,89)],beta=const.beta,myosin=const.myo0)
+    G.add_edges_from([(91,94),(91,92),(91,88),(91,87),(91,90),(91,93)],beta=const.beta,myosin=const.myo0)
+
+    nx.set_node_attributes(G, 'time_lag', 0)
+        
+    centers = [3,8,12,15,20,27,23,30,37,44,33,40,47,54,61,50,57,64,71,67,74,79,82,86,91]
+                
+    for j in centers:
+        G.node[j]['time_lag'] = np.random.randint(0,2000)
+
+    return G
+
+
 
--- MultCell/main4.py
+++ MultCell/main4.py
@@ -15,179 +15,35 @@ import csv
 # Author: Clinton H. Durney
 # Email: cdurney@math.ubc.ca
 #
-# Last Edit: 4/06/17
+# Last Edit: 4/17/17
 #
 # To Do:    
 #
 #
 ###########
 
-# Set-up output file
-BioParamsFile = open('BioParams.csv','w')
-BioParamsWriter = csv.writer(BioParamsFile,delimiter='\t')
-# BioParamsWriter.writerow(["time", "Reg","myo0_1","myo1_17","myo1_7","myo17_13","myo1_10","myo10_17"])
-BioParamsWriter.writerow(["time","cell_0","cell_7","cell_10","cell_13","cell_20","cell_24", "cell_28", "cell_32", "cell_35"])
+centers = [3,8,12,15,20,27,23,30,37,44,33,40,47,54,61,50,57,64,71,67,74,79,82,86,91]
 
-G = nx.Graph()
+# Set-up output file: myosin
+myosinFile = open('myosin.csv','w')
+myosinWriter = csv.writer(myosinFile,delimiter='\t')
+header = ['cell' + str(node) for node in centers]
+header = ['time'] + header
+myosinWriter.writerow(header)
+
+# Set-up output files: cell area
+cellareaFile = open('cellarea.csv','w')
+cellareaWriter = csv.writer(cellareaFile,delimiter='\t')
+cellareaWriter.writerow(header)
+
+# Set-up output files: cell area
+regFile = open('reg.csv','w')
+regWriter = csv.writer(regFile,delimiter='\t')
+regWriter.writerow(header)
+
+# Initialize tissue
+G = tissue()
 H = nx.Graph()
-
-r = const.l_initial
-
-# Left most cell set up
-origin = (0.0,0.0)
-p0 = (r,0.0)
-p1 = (r*np.cos(np.pi/3),r*np.sin(np.pi/3))
-p2 = (r*np.cos(2*np.pi/3),r*np.sin(2*np.pi/3))
-p3 = (r*np.cos(3*np.pi/3),r*np.sin(3*np.pi/3))
-p4 = (r*np.cos(4*np.pi/3),r*np.sin(4*np.pi/3))
-p5 = (r*np.cos(5*np.pi/3),r*np.sin(5*np.pi/3))
-nodes = [p0,p1,p2,p3,p4,p5]
-   
-nodes = [p0,p1,p2,p3,p4,p5]
-i = 0
-G.add_node(i,pos=origin, center=True, phase_angle=np.random.randint(2000), boundary=[1,2,3,4,5,6])
-i += 1 
-for node in nodes:
-    G.add_node(i,pos=node,center=False)
-    i += 1
-    
-# Middle Top Cell with Cap
-origin = 1.5*r, r*np.cos(np.pi/6)
-p1 = (origin[0] + r*np.cos(np.pi/3), origin[1] +r* np.sin(np.pi/3))
-p2 = (origin[0] + r*np.cos(2*np.pi/3), origin[1] + r*np.sin(2*np.pi/3))
-    
-nodes = [p1,p2]
-    
-G.add_node(i,pos=origin,center=True, phase_angle = np.random.randint(2000), boundary=[16,8,9,2,1,17])
-i +=1
-    
-for node in nodes:
-    G.add_node(i,pos=node,center=False)
-    i += 1
-    
-# Middle Bottom Cell with Cap
-origin = 1.5*r, -r*np.cos(np.pi/6)
-p4 = (origin[0] + r*np.cos(4*np.pi/3), origin[1] + r*np.sin(4*np.pi/3))
-p5 = (origin[0] + r*np.cos(5*np.pi/3), origin[1] + r*np.sin(5*np.pi/3))
-nodes = [p4,p5]
-    
-G.add_node(i,pos=origin,center=True,phase_angle = np.random.randint(2000), boundary=[18,17,1,6,11,12])
-i+=1
-    
-for node in nodes:
-    G.add_node(i,pos=node,center=False)
-    i += 1
-    
-# Right most cell
-origin = 1.5*2*r, 0
-p0 = (origin[0] + r, origin[1] + 0)
-p1 = (origin[0] + r*np.cos(np.pi/3), origin[1] + r*np.sin(np.pi/3))
-p2 = (origin[0] + r*np.cos(2*np.pi/3), origin[1] + r*np.sin(2*np.pi/3))
-p3 = (origin[0] + r*np.cos(3*np.pi/3), origin[1] + r*np.sin(3*np.pi/3))
-p4 = (origin[0] + r*np.cos(4*np.pi/3), origin[1] + r*np.sin(4*np.pi/3))
-p5 = (origin[0] + r*np.cos(5*np.pi/3), origin[1] + r*np.sin(5*np.pi/3))
-nodes = [p0,p1,p2,p3,p4,p5]
-    
-G.add_node(i,pos=origin,center=True,phase_angle = np.random.randint(2000), boundary=[14,15,16,17,18,19])
-i+=1
-    
-for node in nodes:
-    G.add_node(i,pos=node,center=False)
-    i += 1
-
-###########################
-origin = 1.5*2*r, 2*r*np.cos(np.pi/6)
-p0 = (origin[0] + r, origin[1] + 0)
-p1 = (origin[0] + r*np.cos(np.pi/3), origin[1] + r*np.sin(np.pi/3))
-p2 = (origin[0] + r*np.cos(2*np.pi/3), origin[1] + r*np.sin(2*np.pi/3))
-nodes = [p0,p1,p2]
-
-G.add_node(i,pos=origin,center=True,phase_angle = np.random.randint(2000), boundary=[21,22,23,8,16,15])
-i+=1
-    
-for node in nodes:
-    G.add_node(i,pos=node,center=False)
-    i += 1
-
-origin = 1.5*2*r, -2*r*np.cos(np.pi/6)
-p0 = (origin[0] + r, origin[1] + 0)
-p4 = (origin[0] + r*np.cos(4*np.pi/3), origin[1] + r*np.sin(4*np.pi/3))
-p5 = (origin[0] + r*np.cos(5*np.pi/3), origin[1] + r*np.sin(5*np.pi/3))
-nodes = [p0,p4,p5]
-
-G.add_node(i,pos=origin,center=True,phase_angle = np.random.randint(2000), boundary=[25,19,18,12,26,27])
-i+=1
-    
-for node in nodes:
-    G.add_node(i,pos=node,center=False)
-    i += 1
-
-origin = 3*(1.5*r), r*np.cos(np.pi/6)
-p0 = (origin[0] + r, origin[1] + 0.0)
-p1 = (origin[0] + r*np.cos(np.pi/3), origin[1] + r*np.sin(np.pi/3))
-p5 = (origin[0] + r*np.cos(5*np.pi/3), origin[1] + r*np.sin(5*np.pi/3))
-nodes = [p0,p1,p5]
-    
-G.add_node(i,pos=origin,center=True, phase_angle = np.random.randint(2000), boundary=[29,30,21,15,14,31])
-i +=1
-    
-for node in nodes:
-    G.add_node(i,pos=node,center=False)
-    i += 1
-
-origin = 3*(1.5*r), -r*np.cos(np.pi/6)
-p0 = (origin[0] + r, origin[1] + 0)
-p5 = (origin[0] + r*np.cos(5*np.pi/3), origin[1] + r*np.sin(5*np.pi/3))
-nodes = [p0,p5]
-
-G.add_node(i,pos=origin,center=True, phase_angle = np.random.randint(2000), boundary=[33,31,14,19,25,34])
-i +=1
-    
-for node in nodes:
-    G.add_node(i,pos=node,center=False)
-    i += 1
-    
-origin = 4*(1.5*r), 0
-p0 = (origin[0] + r, origin[1] + 0)
-p1 = (origin[0] + r*np.cos(np.pi/3), origin[1] + r*np.sin(np.pi/3))
-p5 = (origin[0] + r*np.cos(5*np.pi/3), origin[1] + r*np.sin(5*np.pi/3))
-nodes = [p0,p1,p5]
-
-G.add_node(i,pos=origin,center=True, phase_angle = np.random.randint(2000), boundary=[36,37,29,31,33,38])
-i +=1
-    
-for node in nodes:
-    G.add_node(i,pos=node,center=False)
-    i += 1
-
-###########################
-
-    
-# edges that are only passively elastic
-G.add_path([1,2,3,4,5,6,1],beta=0,myosin=0, color = 'r')
-G.add_path([1,17,16,8,9,2],beta=0,myosin=0, color = 'r')
-G.add_path([6,11,12,18,17,1],beta=0,myosin=0,color = 'r')
-G.add_path([18,19,14,15,16],beta=0,myosin=0, color = 'r')
-G.add_path([15,21,22,23,8],beta=0,myosin=0,color='r')
-G.add_path([12,26,27,25,19],beta=0,myosin=0,color='r')
-G.add_path([25,34,33,31,14],beta=0,myosin=0,color='r')
-G.add_path([31,29,30,21],beta=0,myosin=0,color='r')
-G.add_path([33,38,36,37,29],beta=0,myosin=0,color='r')
-    
-# edges that have active force component
-G.add_edges_from([(0,1),(0,2),(0,3),(0,4),(0,5),(0,6)],beta=const.beta,myosin=const.myo0)
-G.add_edges_from([(7,16),(7,8),(7,9),(7,2),(7,1),(7,17)],beta=const.beta,myosin=const.myo0)
-G.add_edges_from([(10,18),(10,17),(10,1),(10,6),(10,11),(10,12)],beta=const.beta,myosin=const.myo0)
-G.add_edges_from([(13,14),(13,15),(13,16),(13,17),(13,18),(13,19)],beta=const.beta,myosin=const.myo0)
-
-G.add_edges_from([(20,21),(20,22),(20,23),(20,8),(20,16),(20,15)],beta=const.beta,myosin=const.myo0)
-G.add_edges_from([(24,25),(24,19),(24,18),(24,12),(24,26),(24,27)],beta=const.beta,myosin=const.myo0)
-G.add_edges_from([(32,33),(32,31),(32,14),(32,19),(32,25),(32,34)],beta=const.beta,myosin=const.myo0)
-G.add_edges_from([(28,29),(28,30),(28,21),(28,15),(28,14),(28,31)],beta=const.beta,myosin=const.myo0)
-G.add_edges_from([(35,36),(35,37),(35,29),(35,31),(35,33),(35,38)],beta=const.beta,myosin=const.myo0)
-
-
-history = [G.copy()]
 
 # Initial conditions of Biochemical parameters
 Ac = const.Ac0
@@ -200,49 +56,66 @@ AR = const.AR0
 
 # Run dde solver for the Biochemical concentrations
 dt = 0.1
-tf = 6000
+tf = 10000
 (t,Ac,Am,Bc,Bm,Rm,AB,AR) = dde_initializer(Ac,Am,Bc,Bm,Rm,AB,AR,tf,dt)
-
 
 pic_num = 0 
 for index in range(0,len(t)):
     H = G.copy() 
-    ## Update myosin concentration on each spoke ##
-    myo_list = []
-    for center in G.nodes_iter(data=True):
-        if center[1]['center']==True:
-            # Calculate cell area
-            outer = [G.node[element]['pos'] for element in center[1]['boundary']]
-            cell_area = CellArea(outer)
-            
-            myosin_total = 0        # zero total myosin before continuing to next cell
-            
-            for neighbor in G.neighbors(center[0]):
-                # Calculate area of adjacent triangles of the spoke    
-                inner = [G.node[neighbor]['pos']]
-                temp = list(set(G.neighbors(center[0])) & set(G.neighbors(neighbor)))
-                inner.append(G.node[temp[0]]['pos'])
-                inner.append(center[1]['pos'])
-                inner.append(G.node[temp[1]]['pos'])
-                spoke_area = CellArea(inner)
-                geo_frac = spoke_area/cell_area
-                    
-                # Calculate necessary parameters for dm/dt
-                length = distance.euclidean(G.node[center[0]]['pos'],G.node[neighbor]['pos'])
-                myosin_current = G[center[0]][neighbor]['myosin']
-                
-                # Sum the total myosin in the current cell
-                myosin_total += myosin_current
-
-                #update myosin on this edge
-                G[center[0]][neighbor]['myosin'] = dmyosin(myosin_current, geo_frac*Rm[index-center[1]['phase_angle']], length, dt)
-            
-#            print(center[0], myosin_total)
-            myo_list.append(myosin_total)
     
-#    print(myo_list)
-    BioParamsWriter.writerow([t[index],myo_list[0],myo_list[1],myo_list[2],myo_list[3],myo_list[4],myo_list[5],myo_list[6],myo_list[7],myo_list[8]])
+    nodes = nx.get_node_attributes(G,'pos')
+    
+    ## Update myosin concentration on each spoke ##
+    myo_hist = []
+    area_hist = []
+    reg_hist = []
 
+    for n in centers:
+        myosin_total = 0        # zero total myosin before continuing to next cell
+        
+        # Calculate cell area for the nth cell
+        corners = [neighbor for neighbor in G.neighbors(n)]
+        corn_sort = [(corners[0],0)]
+        u = unit_vector(nodes[n],nodes[corners[0]])
+        for i in range(1,len(corners)):
+            v = unit_vector(nodes[n],nodes[corners[i]])
+            dot = np.dot(u,v)
+            det = np.linalg.det([u,v])
+            angle = np.arctan2(det,dot)
+            corn_sort.append((corners[i],angle))
+        corn_sort = sorted(corn_sort, key=lambda tup: tup[1])
+        corn2 = [nodes[entry[0]] for entry in corn_sort]
+        
+        cell_area = CellArea(corn2)
+        
+        for j in range(0,len(corn2)):
+            # Calculate area of adjacent triangles of the spoke    
+            inner = [corn2[np.mod(j,6)],corn2[np.mod(j+1,6)],nodes[n],corn2[np.mod(j-1,6)]]
+             
+            spoke_area = CellArea(inner)
+            geo_frac = spoke_area/cell_area
+            
+            # Calculate necessary parameters for dm/dt
+            length = distance.euclidean(nodes[n],corn2[j])
+            myosin_current = G[n][corn_sort[j][0]]['myosin']
+            
+            #update myosin on this edge
+            if (index - G.node[n]['time_lag']) >= 0:
+                Reg = Rm[index-G.node[n]['time_lag']]
+            else:
+                Reg = 0
+            G[n][corn_sort[j][0]]['myosin'] = dmyosin(myosin_current, geo_frac*Reg, length, dt)
+
+            # Sum the total myosin in the current cell
+            myosin_total += G[n][corn_sort[j][0]]['myosin']
+
+        myo_hist.append(myosin_total)
+        area_hist.append(cell_area)
+        reg_hist.append(Reg)
+
+    myosinWriter.writerow([t[index]] + myo_hist)
+    cellareaWriter.writerow([t[index]] + area_hist)
+    regWriter.writerow([t[index]] + reg_hist)
 
     ## Update force ##
     # iterate over all nodes in graph
@@ -257,10 +130,8 @@ for index in range(0,len(t)):
             mag_force = calc_force(length,G[point][neighbor]['myosin'])
             total_force = np.sum([total_force,mag_force*np.array(dir_vector)],axis=0)
 
-        if point not in [2,3,4,5,6,11,12,26,27,25,34,33,38,36,37,29,30,21,22,23,8,9]:
+        if point not in [0,1,4,5,9,10,16,17,24,25,34,42,51,59,68,76,83,88,92,94,93,90,89,85,84,78,77,70,69,60,52,43,35,26,18,11,6,2]:
             G.node[point]['pos'] = d_pos(H.node[point]['pos'],total_force, dt)
-
-#    BioParamsWriter.writerow([t[index], Rm[index], G[0][1]['myosin'], G[1][17]['myosin'], G[1][7]['myosin'], G[17][13]['myosin'], G[1][10]['myosin'],G[10][17]['myosin']])
 
     if index % 10 == 0:
         pic_num += 1
@@ -271,8 +142,6 @@ for index in range(0,len(t)):
         edges,colors = zip(*nx.get_edge_attributes(G,'color').items())
         nx.draw(G,pos, node_size = 1, edgelist=edges,edge_color=colors,width=1)
         
-        plt.xlim(-20,70)
-        plt.ylim(-25,25)
         plt.axis("on")
         plt.grid("on")
         plt.suptitle("t = %s"%t[index])
