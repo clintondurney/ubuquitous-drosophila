e7d197a08d05a9832cd0d1e8a8c966998b0e9935
Clinton H. Durney <cdurney89@gmail.com>
2018-04-25 11:48:24 -0700

    fix AC

--- MultCell/main4.py
+++ MultCell/main4.py
@@ -142,12 +142,12 @@ for t_index in range(0,len(t)+1):
     ## Actin Cable Formation ##
     # Outdated: Formation follows a Logisitic Curve with params AC_max, k (steepness) and x0 - center of log. curve
     # Updated: Piecewise linear function
-    if t_index*10 < const.x0:
+    if t[t_index] < const.x0:
         for j in range(0,len(AS_boundary)):
             G[AS_boundary[j-1]][AS_boundary[j]]['myosin'] = 0 
-    elif t_index*10 >= const.x0 and t_index*10 < (const.x0 + 150*60):
+    elif t[t_index] >= const.x0 and t[t_index] < (const.x0 + 150*60):
         for j in range(0,len(AS_boundary)):
-            G[AS_boundary[j-1]][AS_boundary[j]]['myosin'] = (300/63)*(t_index*10-const.x0) 
+            G[AS_boundary[j-1]][AS_boundary[j]]['myosin'] = (300/63)*(t[t_index]-const.x0) 
     else:
         for j in range(0,len(AS_boundary)):
             G[AS_boundary[j-1]][AS_boundary[j]]['myosin'] = const.AC_max 
